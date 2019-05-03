a81d1797d9f2886a6e2db7e534de8365eb779a17
katewayc <kate.bqool@gmail.com>
2019-05-03 17:24:20 +0800

    shows necessary data

--- source/ASP_NET_MVC_Q6/ActionFilter/ActualRouteActionFilterAttribute.cs
+++ source/ASP_NET_MVC_Q6/ActionFilter/ActualRouteActionFilterAttribute.cs
@@ -33,14 +33,44 @@ namespace ASP_NET_MVC_Q6.ActionFilter
 
         private void SetOutputString(RouteData rd)
         {
-            var area = rd.DataTokens["area"];
-            var controller = rd.Values["controller"];
-            var action = rd.Values["action"];
-            var id = rd.Values["id"];
-            var page = rd.Values["page"];
-            var category = rd.Values["category"];
-            var OutputString = String.Format("Area: {0} </br> Controller: {1}</br> Action: {2} </br> Id: {3}</br> page: {4}</br> category: {5}</br></br></br></br>", area, controller, action,id,page, category);
-            output.WriteLine(OutputString.ToString());
+            var area = rd.DataTokens["area"]??"";
+            var controller = rd.Values["controller"] ?? "";
+            var action = rd.Values["action"] ?? "";
+            var id = rd.Values["id"] ?? "";
+            var page = rd.Values["page"] ?? "";
+            var category = rd.Values["category"] ?? "";
+
+            string _outputString = "";
+            if (!string.IsNullOrEmpty(area.ToString()))
+            {
+                _outputString += string.Format("Area: {0} </br> ", area);
+            }
+            if (!string.IsNullOrEmpty(controller.ToString()))
+            {
+                _outputString += string.Format("Controller: {0} </br> ", controller);
+            }
+            if (!string.IsNullOrEmpty(action.ToString()))
+            {
+                _outputString += string.Format("Action: {0} </br> ", action);
+            }
+            if (!string.IsNullOrEmpty(id.ToString()))
+            {
+                _outputString += string.Format("Id: {0} </br> ", id);
+            }
+            if (!string.IsNullOrEmpty(page.ToString()))
+            {
+                _outputString += string.Format("page: {0} </br> ", page);
+            }
+            if (!string.IsNullOrEmpty(category.ToString()))
+            {
+                _outputString += string.Format("category: {0} </br> ", category);
+            }
+
+            _outputString += @" </br> </br> </br> </br>";
+            output.WriteLine(_outputString);
+
+            //var OutputString = string.Format("Area: {0} </br> Controller: {1}</br> Action: {2} </br> Id: {3}</br> page: {4}</br> category: {5}</br></br></br></br>", area, controller, action,id,page, category);
+            //output.WriteLine(OutputString.ToString());
         }
     }
 }
